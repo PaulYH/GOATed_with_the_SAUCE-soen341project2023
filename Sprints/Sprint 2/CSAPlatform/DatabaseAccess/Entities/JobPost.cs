d8547284d9c61047e99e2325ee9d72e10f5ce3c4
PaulYH <paulhumenny@icloud.com>
2023-02-14 22:33:55 -0500

    Create entities and relevant enums

---
+++ Sprints/Sprint 2/CSAPlatform/DatabaseAccess/Entities/JobPost.cs
@@ -0,0 +1,21 @@
+﻿using DatabaseAccess.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAccess.Entities
+{
+    public class JobPost
+    {
+        public int Id { get; set; }
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public DateTime ExpirationDate { get; set; }
+        public PostStatusType Status { get; set; } = PostStatusType.Open;
+        public string Location { get; set; } = null!;
+        public JobType JobType { get; set; }
+        public decimal Salary { get; set; }
+
+    }
+}
---
+++ Sprints/Sprint 2/CSAPlatform/DatabaseAccess/Enums/AppStatusType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAccess.Enums
+{
+    public enum AppStatusType
+    {
+        Submitted,
+        Selected,
+        Rejected
+    }
+}
