cebf5a754a1fb8b75f83ec36ad543eb09bc7cfc1
PaulYH <paulhumenny@icloud.com>
2023-03-01 15:36:44 -0500

    Create ApplicationUserService + Connect User Profile to Db

---
+++ Sprints/Sprint 2/CSAPlatform/DatabaseAccess/Data/ApplicationUserService.cs
@@ -0,0 +1,31 @@
+﻿using DatabaseAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAccess.Data
+{
+    public class ApplicationUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApplicationUserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ApplicationUser GetUserById(string userId)
+        {
+            ApplicationUser user = _context.Users.FirstOrDefault(x => x.Id == userId);
+            return user;
+        }
+
+        public async Task UpdateUser(ApplicationUser updatedUser)
+        {
+            _context.Users.Update(updatedUser);
+            _context.SaveChanges();
+        }
+    }
+}
